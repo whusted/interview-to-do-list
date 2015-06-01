d72ac60022433500d8ab0de9f7fb2045e06a881c
HaugL <lianne.haug@colorado.edu>
2015-06-01 16:54:29 -0600

    First commit

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+var Geneva = new Marionette.Application();
+
+Geneva.addRegions({
+  mainRegion: "#main-region"
+});
+
+Geneva.on("start", function() {
+  Backbone.history.start();
+
+  Geneva.mainRegion.show(new Geneva.TestView())
+});
+
+
+Geneva.TestView = Marionette.LayoutView.extend({
+  template: "#test-view",
+  initialize: function(options) {
+    console.log("Hi there! I'm the initialize. No html has been rendered yet")
+  },
+  onRender: function(){
+    console.log("Hi there! I'm the render. All the html has been rendered")
+  },
+  templateHelpers: function() {
+    return {
+      tempVariable: "This is how you pass variables to the template"
+    };
+  }
+});
